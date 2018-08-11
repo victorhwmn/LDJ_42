1102b5d7f0071fb3fab7419eacc33dc1c355bb5b
donOnerb <breno.oliveira@dcomp.sor.ufscar.br>
2018-08-11 15:38:09 -0300

    Tá espawnando o bagulho

--- Timer.gd
+++ Timer.gd
@@ -1,13 +1,16 @@
 extends Timer
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var obj
 var x
 var y
 var TrashMatrix = [[],[],[],[],[]]
 var level = 1
 var trashcount = 0
+var spawn_itens = [
+	preload("res://Losangulo.tscn"),
+	preload("res://Triangulo.tscn"),
+	preload("res://Quadrado.tscn")]
+
 func _ready():
 	#quando o tempo acaba brota mais lixo
 	connect("timeout",self, "_brota_lixo")
@@ -18,22 +21,37 @@ func _brota_lixo():
 	while a != true:
 		_gera_coordenada()
 		#verifica se existe algo na matriz ou se o número gerado já existe
-		print(x)
-		print(y)
 		if TrashMatrix[x].empty() == true or TrashMatrix[x].find(y, 0) == -1:
 			a = true
 			TrashMatrix[x].push_back(y)
 			_instancia_lixo()
+	trashcount += 1
+	if trashcount % 20 == 0:
+		trashcount = 0
+		_level_up()
+	
 	
 func _gera_coordenada():
 	randomize()
 	x = randi()%5
+	randomize()
 	y = randi()%7
+	randomize()
+	obj = randi()%3
 	
 func _level_up():
+	level += 1
+	#subir a dificuldade
+	
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-	pass
+#	pass
+	
 func _instancia_lixo():
-	print(x, y)
+	var item = spawn_itens[obj].instance()
+	item.set_position(Vector2((80*x)+120,(96*y)+144))
+	get_node("/root/MainGame").add_child(item)
+	#converter coordenada gerada para a coordenada do jogo
+	#instanciar o objeto
+	
